58121868393343105843fdc7e57d2b31a928967d
Alina <alechka9906@gmail.com>
2018-02-05 13:32:00 +0000

    Set on child added listener on refKeyWord to mySQL query

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var requestUrl = 'https://flowxo.com/hooks/a/b5vvpb5e';
 var mySQL_business;
 var mySQL_persons;
+var constants = require("./constants.js");
 
 var admin = require("firebase-admin");
 const request = require('request');
@@ -17,5 +18,33 @@ var db = admin.database();
 var refKeyWord = db.ref("keywordfinder");
 
 refKeyWord.orderByChild("status").equalTo("new").on("child_added", function(snapshot, prevChildKey) {
-    
+    var post = snapshot.val();
+    var sqlResult;
+  console.log("vars completed");
+  
+  setConnection();
+  
+  mySQL_persons.connect(function(err) {
+    if(err) throw console.log(err);
+    console.log("mySQL_persons connected!");
+    if(post.message){
+      mySQL_persons.query(post.message, function(err, result) {
+        if(err) throw err;
+        console.log(result);
+          sqlResult = result;
+          updateFirebaseData(snapshot, sqlResult);
+          sendSqlResultToFlowXO(snapshot, sqlResult);
+      });
+    }
+  });  
 });
+
+function setConnection(argument) {
+  var mysql = require('mysql');
+  mySQL_persons = mysql.createConnection({
+      host: constants.MYSQL_HOST_PERSONS,
+      user: constants.MYSQL_USER_PERSONS,
+      password: constants.MYSQL_PASSWORD_PERSONS,
+      database: "spider"
+  });  
+}
