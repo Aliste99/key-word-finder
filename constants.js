1e45985d948b7e3df225f35e429358981f534fbe
Alina <alechka9906@gmail.com>
2018-02-05 10:55:50 +0000

    Create file constants and add constants to .gitignore

---
+++ constants.js
@@ -0,0 +1,10 @@
+function define(name, value) {
+    Object.defineProperty(exports, name, {
+        value:      value,
+        enumerable: true
+    });
+}
+
+define("MYSQL_HOST_PERSONS", "35.184.139.60");
+define("MYSQL_USER_PERSONS", "reader");
+define("MYSQL_PASSWORD_PERSONS", "xbnfntkm");
